a7f7be26b3dd083f1d4367e29bd04037f8de3390
Vinicios Macario <JackWyus_01@outlook.com>
2023-06-10 17:08:01 -0300

    Dados bancários capturados e manipulados

--- Data/ReadData/Read.cs
+++ Data/ReadData/Read.cs
@@ -1,9 +1,35 @@
 public class Read
 {
-  public static void ReadData(){
-    
-    
-    string[] linhas = File.ReadAllLines("../../../Data/Contas.txt");
+  public static List<Cliente> ReadData(){
+  
+    List<Cliente> list = new List<Cliente>();
 
-   }
+
+    string[] linhas = File.ReadAllLines("../../../Data/Contas.txt");
+    
+      foreach(string line in linhas)
+      {
+        string[] parts = line.Split(",");
+            list.Add(new Cliente
+                {
+                    numeroDaAgencia = parts[0],
+                    numeroDaConta   = parts[1],
+                    saldo           = parts[2],
+                    Titular         = parts[3]
+                }
+            );;
+
+      }
+
+        return list;
+  }
+
+    public class Cliente
+    {
+        public string? numeroDaAgencia   { get; set; }
+        public string? numeroDaConta     { get; set; }
+        public string? saldo             { get; set; }
+        public string? Titular           { get; set; }
+    }
+
 }
