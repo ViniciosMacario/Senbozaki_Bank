e86cd0bf1c93483595f92a9908ea1e827084d3b1
Vinicios Macario <JackWyus_01@outlook.com>
2023-06-19 22:01:05 -0300

    Refatorado Código para Respeitar mais o príncipio Solid

--- Program.cs
+++ Program.cs
@@ -2,10 +2,4 @@
 using Senbozaki_Bank.Sistema_de_Login;
 using Senbozaki_Bank.SistemaDeLogin;
 
-//Menu.GeracaoDeOpcoesMaster();
-Console.WriteLine("");
-Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine("-----------------------------------------------------------------------------------------------------------------");
-Console.WriteLine(Cryptography.CryptographyPassword("Teste123"));
-Console.WriteLine("-----------------------------------------------------------------------------------------------------------------");
-Console.WriteLine("");
+Menu.GeracaoDeOpcoesMaster();
