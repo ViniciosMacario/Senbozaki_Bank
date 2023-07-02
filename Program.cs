504696603cc0f3d6c133f2cdbfcbb19cdd8b95b3
Vinicios Macario <JackWyus_01@outlook.com>
2023-07-02 11:42:52 -0300

    Up

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using Senbozaki_Bank.Menu;
 using Senbozaki_Bank.Sistema_de_Login;
-using System.Data;
-using System.Data.SqlClient;
-
-
 Menu.GeracaoDeOpcoesMaster();
 Console.WriteLine(Cryptography.CryptographyPassword("TESTE123"));
